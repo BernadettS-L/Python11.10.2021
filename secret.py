ce0b172c2c49b15d383d68a3fd0f8a77c2d8ebb8
Betti <lencse.bernadett@gmail.com>
2021-10-14 17:54:44 +0200

    MOD: adding unit_converter

--- secret.py
+++ secret.py
@@ -1,6 +1,14 @@
 secret_nr = 4
-guess = int(input("Pick a number!"))
-if guess == secret_nr:
-    print("You win!")
-else:
-    print("You lose")
+guess = 0
+
+talalt = "Correct answer"
+hibas = "Incorrect answer"
+
+message = f"Correct answer is {secret_nr}"
+
+while guess != secret_nr:
+    guess = int(input("Pick a number!"))
+    if guess == secret_nr:
+        print(f"{talalt}")
+    else:
+        print(f"{hibas} {message}")
