32a8151d27ef15740d7b6ac7a119111ab8a9c71b
Betti <lencse.bernadett@gmail.com>
2021-10-21 19:57:50 +0200

    MOD: secret number improvement with JSON

--- secret.py
+++ secret.py
@@ -1,23 +1,29 @@
+import json
 import random
-secret_nr = random.randint(1, 30)
+
+secret_nr = random.randint(1,30)
 
 talalt = "Correct answer"
 hibas = "Incorrect answer"
 attempts = 0
 message = f"Correct answer is {secret_nr}"
 
-with open("score.txt","r") as score_file:
-    best_score = int(score_file.read())
-    print("top score: " + str(best_score))
+with open("score_list","r") as score_file:
+    score_list = json.loads(score_file.read())
+    print("top score: " + str(score_list))
+
+score_list.sort()
+print(score_list[:3])
 
 while True:
     guess = int(input("Pick a number!"))
     attempts += 1
 
     if guess == secret_nr:
-        if attempts < best_score:
-            with open("score.txt","w") as score_file:
-                score_file.write(str(attempts))
+        score_list.append(attempts)
+
+        with open("score_list","w") as score_file:
+            score_file.write(json.dumps(score_list))
         print("yes!")
         print("Attempts needed: " +str(attempts))
         break
